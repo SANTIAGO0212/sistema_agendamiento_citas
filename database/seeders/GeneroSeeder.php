fb60fc167d08539f6caf1a8892e5b99b91457b83
SANTIAGO0212 <santiagocj0212@gmail.com>
2026-04-28 17:40:46 -0500

    Se crean los seeders

---
+++ database/seeders/GeneroSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Genero;
+
+class GeneroSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Genero::firstOrCreate(
+            ['nom_genero' => 'Masculino']
+        );
+
+        Genero::firstOrCreate(
+            ['nom_genero' => 'Femenino']
+        );
+
+        Genero::firstOrCreate(
+            ['nom_genero' => 'No Binario']
+        );
+    }
+}
