a1bd3e11ec23614623e4d8c6e2ae1b8663b80e51
SANTIAGO0212 <santiagocj0212@gmail.com>
2026-07-22 19:34:00 -0500

    Nueva tabla ciudades

---
+++ database/migrations/2026_07_23_001123_create_ciudades_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ciudades', function (Blueprint $table) {
+            $table->id();
+            $table->string('cod_ciudad',20);
+            $table->string('nom_ciudad', 200);
+            $table->foreignId('id_departamento')->constrained('departamentos')->onDelete('cascade');
+            $table->boolean('estado')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ciudades');
+    }
+};
